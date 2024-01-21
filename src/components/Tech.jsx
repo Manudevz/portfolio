cb98c26c45e86190e6ead281af099e7b8247614b
manudevz <enmanuelmarcanoservices@gmail.com>
2024-01-21 13:31:44 -0300

    fixed spheres in mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,16 +7,14 @@ import { technologies, img } from "../constants";
 const Tech = () => {
   return (
     <>
-    <div className='hidden sm:flex flex-row flex-wrap justify-center gap-10'>
+    <div className='flex flex-row flex-wrap justify-center gap-2 sm:gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology?.name}>
           <BallCanvas className='bg-secondary' icon={technology?.icon} />
         </div>
       ))}
     </div>
-    <div className="flex flex-wrap justify-center sm:hidden ">
-      <img src={img} alt="" />
-    </div>
+  
     </>
   );
 };
