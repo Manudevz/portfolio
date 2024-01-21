7f2fc23394f1971d41162456fe0607562bf392e0
manudevz <enmanuelmarcanoservices@gmail.com>
2024-01-21 14:44:10 -0300

    x

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,8 +9,8 @@ const Tech = () => {
     <>
     <div className='flex flex-row flex-wrap justify-center gap-2 sm:gap-10'>
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology?.name}>
-          <BallCanvas className='bg-secondary' icon={technology?.icon} />
+        <div className=' w-18 h-18 sm:w-28 sm:h-28' key={technology?.name}>
+          <BallCanvas className= 'bg-secondary' icon={technology?.icon} />
         </div>
       ))}
     </div>
